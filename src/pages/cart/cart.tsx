4a2fd970a207acb3833f0ef3596cddff753a880d
geraldo_domingos <geraldoneto@locus.software>
2023-03-13 22:16:32 -0300

    ✨feature: finish order

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -2,12 +2,15 @@ import Header from 'components/header/header'
 import styles from './cart.module.scss'
 import relogio from '../../assets/inicial.png'
 
-import { useGetCart, useGetTotal } from 'hooks/useCart'
+import { useGetCart, useGetTotal, useRestCart } from 'hooks/useCart'
 import Category from 'components/category/category'
+import { useDispatch } from 'react-redux'
+import { resetCart } from 'store/reducers/cart'
 
 export default function Carrinho() {
   const cart = useGetCart()
   const total = useGetTotal()	
+  const dispatch = useDispatch()
 
   return (
     <div>
@@ -22,6 +25,7 @@ export default function Carrinho() {
 					foto={item.foto}
 					preco={item.preco}
 					titulo={item.titulo}
+					quantidade={item.quantity}
 					inCart={true}
 				/>) 
 			}
@@ -30,6 +34,12 @@ export default function Carrinho() {
 				<strong>Resumo da compra</strong>
 				<span>Subtotal: <strong>R$ {total.toFixed(2)}</strong></span>
 			</div>
+			<button 
+				className={styles.finalizador}
+				onClick={() => dispatch(resetCart())}
+			>
+				Finalizar compra
+			</button>
 		</div>
     </div>
   )
