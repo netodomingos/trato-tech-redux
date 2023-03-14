81f84de8fd6648f98b6aeaca1187dc3c94f94f88
geraldo_domingos <geraldoneto@locus.software>
2023-03-13 22:15:53 -0300

    ✨feature: add search func

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,12 +1,27 @@
 import styles from './search.module.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootStore } from 'interfaces/RootStore'
+import { changeSearch, resetSearch } from 'store/reducers/search'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function Search() {
+  const search = useSelector((state:  RootStore) => state.search)
+  const dispatch = useDispatch()
+  const location = useLocation()
+
+  useEffect(() => {
+    dispatch(resetSearch())
+  }, [location.pathname])
+  
   return (
     <div className={styles.busca}>
         <input 
           className={styles.input} 
           type="text" 
           placeholder='O que você procura?'
+          value={search}
+          onChange={event => dispatch(changeSearch(event.target.value))}
         />
     </div>
   )
