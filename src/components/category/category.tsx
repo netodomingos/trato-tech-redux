faeb52fe3ecb6ed78357854d26ccec8ea155eb18
geraldo_domingos <geraldoneto@locus.software>
2023-03-13 22:16:10 -0300

    ✨feature: counter in cart

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,14 +1,14 @@
 import { CategoryComponent } from 'interfaces/Categorys'
 import styles from './category.module.scss'
-import { IconPropsItem } from '../../constants/constants'
-import { AiFillHeart, AiOutlineHeart, FaCartPlus } from '../../constants/icons'
+import { IconPropsItem, QuantityItemProps } from '../../constants/constants'
+import { AiFillHeart, AiOutlineHeart, FaCartPlus, AiFillMinusCircle, AiFillPlusCircle } from '../../constants/icons'
 import { changeFavourite } from 'store/reducers/itens'
-import { addInCart } from 'store/reducers/cart'
+import { addInCart, changeQuantity, removeFromCart } from 'store/reducers/cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootStore } from 'interfaces/RootStore'
 import classNames from 'classnames'
 
-export default function Category({ titulo, foto, preco, descricao, favorito, id, inCart }: CategoryComponent) {
+export default function Category({ titulo, foto, preco, descricao, favorito, id, inCart, quantidade }: CategoryComponent) {
 	const dispatch = useDispatch()
     const inCartReducer = useSelector((state: RootStore) => state.cart.some(item => item.id === id))
 
@@ -42,13 +42,31 @@ export default function Category({ titulo, foto, preco, descricao, favorito, id,
                         ? <AiFillHeart {...IconPropsItem} color='#ff0000' className={styles['item-acao']} onClick={favourite}/>
                         : <AiOutlineHeart {...IconPropsItem} className={styles['item-acao']} onClick={favourite}/>
                     }
-
-                    <FaCartPlus 
-                        {...IconPropsItem}
-                        color={inCartReducer ? '#1875E8' : IconPropsItem.color}
-                        className={styles['item-acao']}
-                        onClick={add}
-                    />
+                    {
+                        inCart ?  (
+                            <div className={styles.quantidade}>
+                                Quantidade:
+                                <AiFillMinusCircle {...QuantityItemProps} onClick={() => {
+                                    if(quantidade === 1){
+                                        dispatch(removeFromCart(id))
+                                    } else {
+                                        dispatch(changeQuantity({ id: id, quantity: -1 }))
+                                    }
+                                }}/>
+                                <span>{String(quantidade || 0).padStart(2, "0")}</span>
+                                <AiFillPlusCircle {...QuantityItemProps} onClick={() => dispatch(changeQuantity({ id: id, quantity: +1 }))}/>
+                            </div>
+                        )
+                        :  (
+                            <FaCartPlus 
+                                {...IconPropsItem}
+                                color={inCartReducer ? '#1875E8' : IconPropsItem.color}
+                                className={styles['item-acao']}
+                                onClick={add}
+                            />
+                        )
+                    }
+                   
                 </div>
             </div>
         </div>
